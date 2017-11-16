cf8418901ec90f6e25f52324e6a128eee740a29f
Dylan On <dylanon@users.noreply.github.com>
2017-11-16 14:46:03 -0500

    Update pseudocode

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,15 @@
 // - User picks a wiki to search (for MVP, there is only one option)
 // - User enters search text
 // - User submits search text
-// - Send a 'query' request to the API endpoint, pass along the search text
-// - API returns JSON listing the matching pages
-// - Display a list of matching pages
-// - User clicks a page name from the list
-// - Store the clicked page's name
-// - Send a 'parse' request to the API endpoint, pass along the page name
-// - API returns page data as JSON, including the HTML for the page
+// * - Send a 'query' request to the API endpoint, pass along the search text
+// * - API returns JSON listing the matching pages
+// * - Display a list of matching pages
+// * - User clicks a page name from the list
+// * - Store the clicked page's name
+// * - Send a 'parse' request to the API endpoint, pass along the page name
+// * - API returns page data as JSON, including the HTML for the page
 // - Process/sanitize the HTML in some way (there is a lot of junk in there)
-//   - get h1 (document title)
+//   * - get h1 (document title)
 //   - get page content inside div#bodyContent
 //   - Ryan suggests using regex to clean up HTML
 // - Display the sanitized HTML in the content area
